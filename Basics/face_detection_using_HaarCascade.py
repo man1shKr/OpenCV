7331f1b7ab167b91af0dfd9f4b593a7aee6339a9
man1shKr <38997839+man1shKr@users.noreply.github.com>
2019-02-27 19:07:24 +0530

    Add files via upload

---
+++ Basics/face_detection_using_HaarCascade.py
@@ -0,0 +1,36 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jan 29 16:03:38 2019
+
+@author: MANISH
+"""
+
+import cv2
+cap = cv2.VideoCapture(0)
+face_cascade = cv2.CascadeClassifier("haarcascade_frontalface_alt.xml")
+
+while True:
+    ret,frame = cap.read()
+    gray_frame= cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+    
+    if ret==False:
+        continue
+    
+    faces = face_cascade.detectMultiScale(gray_frame,1.3,5)
+    
+    for (x,y,w,h) in faces:
+        cv2.rectangle(frame,(x,y),(x+w,y+h),(0,0,255),2)
+    
+    cv2.imshow("Video Frame",frame)
+    #cv2.imshow("Gray Frame",gray_frame)
+    
+    
+    #wait for user input - q,then u will stop the loop
+    
+    key_pressed = cv2.waitKey(1) & 0xFF
+    if key_pressed ==ord('q'):
+        break
+    
+cap.release()
+cv2.destroyAllWindows()    
+
